709d10bf082d686f7a93809db21fe4f1691b6b29
deazuri <deazuriraihan@gmail.com>
2022-12-05 19:44:34 +0700

    Tugas Servis

---
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.musicplayerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.musicplayerapp.MusicService;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnStart, btnStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,MusicService.class));
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MusicService.class));
+            }
+        });
+
+    }
+}
